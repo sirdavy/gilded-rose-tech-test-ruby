171a9da5dbdf2c10bc9c84939cec5ccae6c19588
David Scott <sirdavidscott@hotmail.com>
2023-02-22 17:26:18 +0000

    tests that the quality of any item, when zero, does not degrade below zero passes

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -21,9 +21,27 @@ describe GildedRose do
       expect(items[1].sell_in).to eq -1
       expect(items[1].quality).to eq 6
     end
+    
+    it "tests that the quality of any item, when zero, does not degrade below zero" do
+      items = [
+        Item.new("Normal item", 2, 0),
+        Item.new("Aged Brie", 10, 0),
+        Item.new("Sulfuras, Hand of Ragnaros", 0, 80),
+        Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 0),
+        #Item.new("Conjured item", 0, 0)
+      ]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to eq 0
+      expect(items[1].quality).to eq 1
+      expect(items[2].quality).to eq 80
+      expect(items[3].quality).to eq 0
+      # expect(items[4].quality).to eq 0
+    
+    end
+
+
 
   end
-
 end
 
 
