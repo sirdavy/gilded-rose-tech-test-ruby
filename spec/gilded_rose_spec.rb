4b7277dc21e18c4f5be680f429f66fdd14f36a90
David Scott <sirdavidscott@hotmail.com>
2023-02-22 18:00:29 +0000

    tests the update of sell_in and quality of 'Backstage passes' passes

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -77,7 +77,28 @@ describe GildedRose do
       expect(items[0].sell_in).to eq 0
       expect(items[0].quality).to eq 80
     end
-    
+
+    it "tests the update of sell_in and quality of Backstage passes" do 
+      items = [
+        Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 10),
+        Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10),
+        Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10),
+        Item.new("Backstage passes to a TAFKAL80ETC concert", 1, 10),
+        Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)
+        ]
+      GildedRose.new(items).update_quality()
+      expect(items[0].sell_in).to eq 14
+      expect(items[0].quality).to eq 11
+      expect(items[1].sell_in).to eq 9
+      expect(items[1].quality).to eq 12
+      expect(items[2].sell_in).to eq 4
+      expect(items[2].quality).to eq 13
+      expect(items[3].sell_in).to eq 0
+      expect(items[3].quality).to eq 13
+      expect(items[4].sell_in).to eq -1
+      expect(items[4].quality).to eq 0
+    end
+
   end
 end
 
