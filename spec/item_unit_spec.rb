f3d7a335434d44eeaa79868cf05d1764c6ab99db
David Scott <sirdavidscott@hotmail.com>
2023-02-23 14:54:38 +0000

    item unit tests pass

--- spec/item_unit_spec.rb
+++ spec/item_unit_spec.rb
@@ -1,10 +1,17 @@
 require 'item'
 
 describe Item do
+  
   it 'constructs an item' do
     item = Item.new("Odd Substance", 10, 10)
     expect(item.name).to eq "Odd Substance"
     expect(item.sell_in).to eq 10
     expect(item.quality).to eq 10
   end
+
+  it 'returns a string with item info' do
+    item = Item.new("Odd Substance", 10, 10)
+    expect(item.to_s).to eq "Odd Substance, 10, 10"
+  end
+
 end
